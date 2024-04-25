fd7a7fea44ff2d9c77ee9bbd76eeada0fd2be3d4
Andrija233 <andrijam007@hotmail.rs>
2024-04-25 20:00:29 +0200

    User-form + update

--- restaurant-app/backend/src/routes/UserRouter.ts
+++ restaurant-app/backend/src/routes/UserRouter.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import UserRouterController from "../controllers/UserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 // router for index
+router.get("/", jwtCheck, jwtParse,  UserRouterController.getCurrentUser);
 router.post("/", jwtCheck, UserRouterController.createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, UserRouterController.updateCurrentUser);
 
 export default router;
