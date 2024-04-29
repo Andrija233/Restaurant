15900e60afb056eb383c83981260ceca4d530480
Andrija233 <andrijam007@hotmail.rs>
2024-04-29 12:39:58 +0200

    Update search filter + pages

--- restaurant-app/frontend/src/api/RestaurantApi.tsx
+++ restaurant-app/frontend/src/api/RestaurantApi.tsx
@@ -1,3 +1,4 @@
+import { SearchState } from "@/pages/SearchPage";
 import { RestaurantSearchResponse } from "@/types";
 import { useQuery } from "react-query";
 
@@ -5,10 +6,14 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 
 
-export const useSearchRestaurants = (city?: string) => {
+export const useSearchRestaurants = (searchState: SearchState, city?: string) => {
 
     const createSearchRequest = async(): Promise<RestaurantSearchResponse> =>{
-        const response = await fetch(`${API_BASE_URL}/api/restaurant/search/${city}`);
+        const params = new URLSearchParams();
+        params.set("searchQuery", searchState.searchQuery);
+        params.set("page", searchState.page.toString());
+
+        const response = await fetch(`${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`);
 
         if(!response.ok){
             throw new Error("Failed to search for restaurants");
@@ -17,7 +22,7 @@ export const useSearchRestaurants = (city?: string) => {
         return response.json();
     }
 
-    const { data:results, isLoading } = useQuery(["searchRestaurants"], createSearchRequest,{
+    const { data:results, isLoading } = useQuery(["searchRestaurants", searchState], createSearchRequest,{
         enabled: !!city
     });
 
--- restaurant-app/frontend/src/components/SearchBar.tsx
+++ restaurant-app/frontend/src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useEffect } from "react";
 
 const formSchema = z.object({
     searchQuery: z.string({
@@ -18,15 +19,22 @@ type Props = {
     onSubmit: (formData: SearchForm) => void
     placeHolder: string;
     onReset?: () => void
+    searchQuery: string
 }
 
-const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
+const SearchBar = ({ onSubmit, onReset, placeHolder, searchQuery }: Props) => {
     const form = useForm<SearchForm>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            searchQuery: "",
+            searchQuery,
         }
     })
+
+    useEffect(() => {
+        form.reset({
+            searchQuery
+        })
+    }, [form, searchQuery])
 
     const handleReset = () => {
         form.reset({
@@ -42,7 +50,7 @@ const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
         <Form {...form}>
             <form 
             onSubmit={form.handleSubmit(onSubmit)} 
-            className={`flex items-center flex-1 gap-3 justify-between flex-row border-2 rounded-full p-3 mx-5 ${form.formState.errors.searchQuery && "border-red-500"}`}
+            className={`flex items-center flex-1 gap-3 justify-between flex-row border-2 rounded-full p-3 ${form.formState.errors.searchQuery && "border-red-500"}`}
             >
                 <Search strokeWidth={2.5} size={30} className="ml-1 text-orange-500 hidden md:block"/>
                 <FormField control={form.control} name="searchQuery" render={({ field }) => (
@@ -52,7 +60,7 @@ const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
                         </FormControl>
                     </FormItem>
                 )}/>
-                {form.formState.isDirty && (<Button type="button" variant="outline" onClick={handleReset} className="rounded-full bg-red-500">Clear</Button>)}
+                <Button type="button" variant="outline" onClick={handleReset} className="rounded-full bg-red-500">Reset</Button>
                 <Button type="submit" className="rounded-full bg-orange-500">Search</Button>
             </form>
         </Form>
