5ae85e1a3f94533b9c35f067cdb672958efbaee5
Andrija233 <andrijam007@hotmail.rs>
2024-05-01 00:41:34 +0200

    Stripe payment finished

--- restaurant-app/backend/src/controllers/OrderController.ts
+++ restaurant-app/backend/src/controllers/OrderController.ts
@@ -5,6 +5,19 @@ import Order from "../models/order";
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
+const STRIPE_ENDPOINT_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string;
+
+
+const getMyOrders = async (req: Request, res: Response) => {
+    try {
+        const orders = await Order.find({ user: req.userId }).populate("restaurant").populate("user");
+        res.json(orders);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message:"Something went wrong" });
+    }
+}
 
 type CheckoutSessionRequest = {
     cartItems:{
@@ -20,6 +33,35 @@ type CheckoutSessionRequest = {
     };
     restaurantId: string;
 }
+
+const stripeWebhookHandler = async (req: Request, res: Response) => {
+    let event;
+
+    try {
+        const sig = req.headers["stripe-signature"];
+        event = STRIPE.webhooks.constructEvent(req.body, sig as string, STRIPE_ENDPOINT_SECRET);
+    } catch (error: any) {
+        console.log(error);
+        return res.status(400).send(`Webhook error: ${error.message}`);
+    }
+
+    if(event.type === "checkout.session.completed") {
+        const order = await Order.findById(event.data.object.metadata?.orderId);
+
+        if(!order) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+
+        order.totalAmount = event.data.object.amount_total;
+        order.status = "paid";
+
+        await order.save();
+    }
+
+    res.status(200).send();
+}
+
+
 
 const createCheckoutSession = async (req: Request, res: Response) => {
     try {
@@ -110,4 +152,4 @@ const createSession = async(lineItems: Stripe.Checkout.SessionCreateParams.LineI
     return sessionData;
 }
 
-export default { createCheckoutSession }
+export default { getMyOrders,createCheckoutSession, stripeWebhookHandler }
